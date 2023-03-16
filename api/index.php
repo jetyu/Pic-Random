8e6eca9e724a5f68a78b55e0e73c80b42c9439c4
jetyu <jetyu@users.noreply.github.com>
2023-03-16 21:08:28 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -7,7 +7,7 @@ if ($handle=opendir("$path")) {
 			if(substr($file,-3)=='png' || substr($file,-3)=='jpg' || substr($file,-4)=='webp') $files[count($files)] = $file;
 			
 		}
-		$random=mt_rand(0,count($files)-1);
+		$random=mt_rand(0,count($files));
 	}
 	closedir($handle);
 	$url="./album/$files[$random]";
