9b3c1abee4f58156b9ed984202204681b5d78b94
jetyu <jetyu@users.noreply.github.com>
2023-03-16 08:33:50 +0000

    random

--- api/index.php
+++ api/index.php
@@ -2,14 +2,16 @@
 $path = $_SERVER['DOCUMENT_ROOT'].'/album';
 $files=array();
 if ($handle=opendir("$path")) {
-while(false !== ($file = readdir($handle))) {
-if ($file != "." && $file != "..") {
-if(substr($file,-3)=='png' || substr($file,-3)=='jpg' || substr($file,-4)=='webp') $files[count($files)] = $file;//带后缀筛选的文件数组
-}
-$random=rand(0,count($files)-1);
-}
-closedir($handle);
-$url="./album/$files[$random]";
-header("Location: $url");//302
+	while(false !== ($file = readdir($handle))) {
+		if ($file != "." && $file != "..") {
+			if(substr($file,-3)=='png' || substr($file,-3)=='jpg' || substr($file,-4)=='webp') $files[count($files)] = $file;
+			
+		}
+		$random=mt_rand(0,count($files));
+	}
+	closedir($handle);
+	$url="./album/$files[$random]";
+	header("Location: $url");
+	
 }
 ?>
