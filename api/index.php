35794c27a1b24dcfd3f6c8c199004f645be8807e
jetyu <jetyu@users.noreply.github.com>
2023-03-17 21:44:11 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -3,13 +3,8 @@
 $path = $_SERVER['DOCUMENT_ROOT'].'/img';
 
 $files=array();
-if ($handle=opendir("$path")) {
-	while(false !== ($file = readdir($handle))) {
-		if ($file != "." && $file != "..") {
-			if(substr($file,-3)=='png' || substr($file,-3)=='jpg' || substr($file,-4)=='webp') $files[count($files)] = $file;
-		
-		}
-		$random=mt_rand(0,count($files));
+	  	
+		random=rand(0,count($files));
 	}
 	closedir($handle);
 	$url="./img/$files[$random]";
