019356b7d5a850fef86116b4232af9197c0322de
jetyu <jetyu@users.noreply.github.com>
2023-03-17 21:45:50 +0800

    Update index.php

--- api/index.php
+++ api/index.php
@@ -3,9 +3,8 @@
 $path = $_SERVER['DOCUMENT_ROOT'].'/img';
 
 $files=array();
-	  	
-		random=rand(0,count($files));
-	}
+$random=rand(0,count($files));
+	
 	closedir($handle);
 	$url="./img/$files[$random]";
   	header("Location: $url");
