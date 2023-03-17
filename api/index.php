1b171b109b07de5691b9a8e8bfde3f2bd6b7b84c
jetyu <jetyu@users.noreply.github.com>
2023-03-17 06:12:05 +0000

    Update index.php

--- api/index.php
+++ api/index.php
@@ -1,6 +1,19 @@
-<?php
-$img_array = glob("album/*.{gif,jpg,png}",GLOB_BRACE); 
-$img = array_rand($img_array); 
-$dz = $img_array[$img];
-header("Location:".$dz);
+<?php 
+
+$path = $_SERVER['DOCUMENT_ROOT'].'/img';
+
+$files=array();
+if ($handle=opendir("$path")) {
+	while(false !== ($file = readdir($handle))) {
+		if ($file != "." && $file != "..") {
+			if(substr($file,-3)=='png' || substr($file,-3)=='jpg' || substr($file,-4)=='webp') $files[count($files)] = $file;
+		
+		}
+		$random=mt_rand(0,count($files)-1);
+	}
+	closedir($handle);
+	$url="./img/$files[$random]";
+	header('HTTP/1.1 301 Moved Permanently'); //无此句则是302
+  header('Location: http://picture.jetyu.me');
+}
 ?>
