a9589c839f6e4a36cafbceea0234a9a06806891d
jetyu <jetyu91@163.com>
2023-03-18 09:44:59 +0800

    09

--- api/index.php
+++ api/index.php
@@ -1,16 +1,16 @@
-<?php 
-//v0.1 🤔
-$path = $_SERVER['DOCUMENT_ROOT'].'/img';//获取相册在主机中的绝对路径
-$files=array();
-if ($handle=opendir("$path")) {
-while(false !== ($file = readdir($handle))) {
-if ($file != "." && $file != "..") {
-if(substr($file,-3)=='png' || substr($file,-3)=='jpg' || substr($file,-4)=='webp') $files[count($files)] = $file;//带后缀筛选的文件数组
-}
-$random=rand(0,count($files)-1);
-}
-closedir($handle);
-$url="./img/$files[$random]";
-header("Location: $url");//302
-}
-?>
+<? php
+
+$path = $_SERVER['DOCUMENT_ROOT'].
+'/img';
+$files = array();
+if ($handle = opendir("$path")) {
+    while (false !== ($file = readdir($handle))) {
+        if ($file != "." && $file != "..") {
+            if (substr($file, -3) == 'png' || substr($file, -3) == 'jpg') $files[count($files)] = $file; 
+        }
+        $random = rand(0, count($files) - 1);
+    }
+    closedir($handle);
+    $url = "./img/$files[$random]";
+    header("Location: $url");
+} ?>
