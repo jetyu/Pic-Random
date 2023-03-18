b9f56e7e70615c5a4cb779e437faee2681ad33c5
jetyu <jetyu91@163.com>
2023-03-18 10:04:48 +0800

    123

--- api/index.php
+++ api/index.php
@@ -1,16 +1,16 @@
-<? php
+<?php 
 
 $path = $_SERVER['DOCUMENT_ROOT'].'/img';
-$files = array();
-if ($handle = opendir("$path")) {
-    while (false !== ($file = readdir($handle))) {
-        if ($file != "." && $file != "..") {
-            if (substr($file, -3) == 'png' || substr($file, -3) == 'jpg') $files[count($files)] = $file; 
-        }
-        $random = rand(0, count($files) - 1);
-    }
-    closedir($handle);
-    $url = "./img/$files[$random]";
-    header("Location: $url");
-} 
+$files=array();
+if ($handle=opendir("$path")) {
+while(false !== ($file = readdir($handle))) {
+if ($file != "." && $file != "..") {
+if(substr($file,-3)=='png' || substr($file,-3)=='jpg') $files[count($files)] = $file;
+}
+$random=rand(0,count($files)-1);
+}
+closedir($handle);
+$url="./img/$files[$random]";
+header("Location: $url");//302
+}
 ?>
